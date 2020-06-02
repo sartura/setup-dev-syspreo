5f76ffed2b8c1dad6f9683c32a93a932ba8a5752
Jakov Petrina <jakov.petrina@sartura.hr>
2020-06-02 11:52:57 +0200

    setup-dev-sysrepo: paralellize as much as possible
    Signed-off-by: Jakov Petrina <jakov.petrina@sartura.hr>

--- setup-dev-sysrepo.sh
+++ setup-dev-sysrepo.sh
@@ -23,7 +23,7 @@ sed -i 's,LIBRARY DESTINATION lib,LIBRARY DESTINATION lib64,' CMakeLists.txt
 sed -i 's,ARCHIVE DESTINATION lib,ARCHIVE DESTINATION lib64,' CMakeLists.txt
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DBUILD_SHARED_LIBS=ON -DPCRE_SUPPORT_UTF=ON -DPCRE_SUPPORT_UNICODE_PROPERTIES=ON -DPCRE_SUPPORT_VALGRIND=ON ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -33,7 +33,7 @@ echo
 git clone https://github.com/CESNET/libyang.git && cd libyang
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -43,7 +43,7 @@ echo
 git clone https://github.com/sysrepo/sysrepo.git && cd sysrepo
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug -DREPO_PATH=$SYSREPO_DIR/etc/sysrepo ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -54,7 +54,7 @@ git clone http://git.libssh.org/projects/libssh.git && cd libssh
 git checkout libssh-0.9.2
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -64,7 +64,7 @@ echo
 git clone https://github.com/openssl/openssl.git && cd openssl
 git checkout OpenSSL_1_1_1-stable
 ./config --prefix=$SYSREPO_DIR --openssldir=$SYSREPO_DIR/etc
-make && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -74,7 +74,7 @@ echo
 git clone https://github.com/CESNET/libnetconf2.git && cd libnetconf2
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -91,8 +91,8 @@ sed -i "/OPENSSL=/c\\OPENSSL=${SYSREPO_DIR}/bin/openssl" scripts/merge_hostkey.s
 sed -i "/SYSREPOCFG=/c\\SYSREPOCFG=${SYSREPO_DIR}/bin/sysrepocfg" scripts/merge_config.sh
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug -DPIDFILE_PREFIX=$SYSREPO_DIR/var/run ..
-make -j2
-LD_LIBRARY_PATH=$SYSREPO_DIR/lib make install
+make -j$(nproc)
+LD_LIBRARY_PATH=$SYSREPO_DIR/lib make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -102,7 +102,7 @@ echo
 git clone https://github.com/json-c/json-c.git && cd json-c
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -111,7 +111,7 @@ echo
 git clone git://git.cryptomilk.org/projects/cmocka.git && cd cmocka
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -121,7 +121,7 @@ echo
 git clone https://git.lede-project.org/project/libubox.git && cd libubox
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX:PATH=$SYSREPO_DIR -DBUILD_LUA=OFF -DBUILD_EXAMPLES=OFF ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -131,7 +131,7 @@ echo
 git clone https://git.lede-project.org/project/ubus.git && cd ubus
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$SYSREPO_DIR -DBUILD_LUA=OFF -DBUILD_EXAMPLES=OFF ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -141,7 +141,7 @@ echo
 git clone https://git.lede-project.org/project/uci.git && cd uci
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DBUILD_LUA=OFF ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -151,7 +151,7 @@ echo
 git clone git://git.openwrt.org/project/rpcd.git && cd rpcd
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$SYSREPO_DIR -DFILE_SUPPORT=OFF -DRPCSYS_SUPPORT=OFF -DIWINFO_SUPPORT=OFF ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories
 
 echo
@@ -168,7 +168,7 @@ echo
 git clone git@github.com:sartura/srpo.git && cd srpo
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug -DUCI_CONFIG_DIR="$SYSREPO_DIR/etc/config" ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -179,7 +179,7 @@ git clone git@github.com:sartura/sr_uci.git && cd sr_uci
 git checkout devel
 mkdir build && cd build
 cmake -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2 && make install
+make -j$(nproc) && make -j$(nproc) install
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -190,7 +190,7 @@ git clone git@github.com:sartura/dhcp.git && cd dhcp
 git checkout devel
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -201,7 +201,7 @@ git clone git@github.com:sartura/provisioning-plugin.git && cd provisioning-plug
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -212,7 +212,7 @@ git clone git@github.com:sartura/firmware-plugin.git && cd firmware-plugin
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -223,7 +223,7 @@ git clone git@github.com:sartura/wireless-plugin.git && cd wireless-plugin
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -234,7 +234,7 @@ git clone git@github.com:sartura/sip-plugin.git && cd sip-plugin
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -245,7 +245,7 @@ git clone git@github.com:sartura/network-plugin.git && cd network-plugin
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -256,7 +256,7 @@ git clone git@github.com:sartura/generic-ubus-plugin.git && cd generic-ubus-plug
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -273,7 +273,7 @@ git clone git@github.com:sartura/firmware-plugin-openwrt.git && cd firmware-plug
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -284,7 +284,7 @@ git clone git@github.com:sartura/wireless-plugin-openwrt.git && cd wireless-plug
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
@@ -295,7 +295,7 @@ git clone git@github.com:sartura/network-plugin-openwrt.git && cd network-plugin
 git checkout sysrepo-1.x.x
 mkdir build && cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH=$SYSREPO_DIR -DCMAKE_INSTALL_PREFIX=$SYSREPO_DIR -DCMAKE_BUILD_TYPE=Debug ..
-make -j2
+make -j$(nproc)
 cd $SYSREPO_DIR/repositories/plugins
 
 echo
